2ab3f24e5ffbfe64b677168fb6e6f87ed59b7205
Thomas Segismont <tsegismo@redhat.com>
2019-08-13 14:25:36 +0200

    wrk script for author profile mobile query

---
+++ author-profile-mobile.lua
@@ -0,0 +1,11 @@
+wrk.method = "POST"
+wrk.headers["Accept"] = "application/json"
+wrk.headers["Content-Type"] = "application/json"
+
+counter = 1
+
+request = function()
+   body = [[{"query":"query ($authorId: Int!) {\n  author(id: $authorId) {\n    firstName\n    lastName\n    bio\n    posts {\n      title\n    }\n  }\n}","variables":{"authorId": ]]..counter..[[}}]]
+   counter = counter < 10 and (counter + 1) or 1
+   return wrk.format(nil, nil, nil, body)
+end

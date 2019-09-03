4184efb1d0c463ff8ab8161e7d5b4c5eedcc1ea9
Thomas Segismont <tsegismo@redhat.com>
2019-09-03 12:48:54 -0400

    Added Postgres start script

---
+++ postgres/run-postgresql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo docker run --network host -it graphql-server-benchmark/postgres \
+	-c "listen_addresses=*" \
+	-c "max_connections=2000" \
+	-c "shared_buffers=256MB" \
+	-c "work_mem=64MB" \
+	-c "maintenance_work_mem=512MB" \
+	-c "checkpoint_completion_target=0.9" \
+	-c "effective_cache_size=8GB" \
+	-c "random_page_cost=2"

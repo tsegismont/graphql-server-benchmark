f9bb73c91ac22730bdbcec555700b22ed17c6934
Thomas Segismont <tsegismo@redhat.com>
2020-04-02 12:04:06 +0200

    Add benchmark tests for 512 and 1024 connections

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -23,7 +23,7 @@ DURATION="1m"
 echo "Warming up framework ${FRAMEWORK_DIR} for benchmark ${LUA_FILE}..."
 ./warmup-server.sh "${LUA_FILE}"
 
-for connections in 8 16 32 64 128 256; do
+for connections in 8 16 32 64 128 256 512 1024; do
   echo "Running benchmark ${LUA_FILE} for framework ${FRAMEWORK_DIR} with $connections connections..."
   ./stress-server.sh "${LUA_FILE}" $connections $DURATION "$REPORT_FILE"
 done

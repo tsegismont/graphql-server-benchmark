6fbf00232d781f49b4ca602097486c635d3bf3c0
Thomas Segismont <tsegismo@redhat.com>
2020-04-06 14:48:06 +0200

    Create as many instances as event loops

--- Java/vertx-graphql-java/run-bare-server.sh
+++ Java/vertx-graphql-java/run-bare-server.sh
@@ -5,4 +5,4 @@ set -e
 trap "exit" INT
 
 mvn clean package
-java -Xms2G -Xmx2G -server -jar target/vertx-graphql-java.jar -instances 8 -conf src/main/conf/server.json
+java -Xms2G -Xmx2G -server -jar target/vertx-graphql-java.jar -instances $((2 * $(grep --count ^processor /proc/cpuinfo))) -conf src/main/conf/server.json
